3fb7ec0b875cf121118ae7011062f185968eab03
Gabriel <gabriel.pagotto@icloud.com>
2022-06-10 22:49:20 -0300

    add examples

--- mock.py
+++ mock.py
@@ -2,16 +2,15 @@ import os, flask, json
 from fileinput import close
 
 class AvailableEndpoint:
-    def __init__(self, folder_path: str, file_path: str, url: str, filename: str):
-        self.folder_path = folder_path
+    def __init__(self, dir_path: str, file_path: str, url: str, filename: str):
+        self.dir_path = dir_path
         self.file_path = file_path
         self.url = url
         self.filename = filename
-        self.callback_method_name = url + filename
-        
+        self.__data_callback_name = url + filename
 
-    def get_json_callback(self) -> flask.Response:
-        os.chdir(self.folder_path)
+    def data_callback(self) -> flask.Response:
+        os.chdir(self.dir_path)
 
         with open(self.filename, 'r') as file_object:
             json_string = file_object.read()
@@ -19,20 +18,21 @@ class AvailableEndpoint:
             close()       
             return flask.jsonify(json_o)
 
-    def set_callback_name(self):
-        handler = self.get_json_callback
-        handler.__func__.__name__ = self.callback_method_name
+    def handler_data_callback(self):
+        handler = self.data_callback
+        handler.__func__.__name__ = self.__data_callback_name
 
 class MockServer:
     app = flask.Flask(__name__)
     available_endpoints = list()
 
     def run(self):
-        self.load_available_endpoints(True)
-        self.set_paths_in_app()
+        self.__load_available_endpoints(True)
+        self.__set_main_view()
+        self.__set_endpoints()
         self.app.run()
 
-    def load_available_endpoints(self, is_first_call = False):
+    def __load_available_endpoints(self, is_first_call = False):
         if is_first_call:
             os.chdir('mocks')
 
@@ -50,19 +50,20 @@ class MockServer:
                 try:
                     current_path = os.getcwd()
                     os.chdir(fd)
-                    self.load_available_endpoints()            
+                    self.__load_available_endpoints()            
                     os.chdir(current_path)
 
                 except:
                     is_json_file = fd.endswith(".json")
                     is_html_file = fd.endswith(".html")
+                    
+                    folder_path = os.getcwd()
+                    file_path =  "{0}/{1}".format(folder_path.split("/mocks")[1], fd)
 
                     if is_json_file:
-                        folder_path = os.getcwd()
-                        file_path =  "{0}/{1}".format(folder_path.split("/mocks")[1], fd)
                         route_path = file_path.replace(".json", "/")
                         available_endpoint = AvailableEndpoint(
-                            folder_path=folder_path,
+                            dir_path=folder_path,
                             file_path=file_path,
                             url=route_path,
                             filename=fd)
@@ -72,11 +73,17 @@ class MockServer:
                     elif is_html_file:
                         pass
 
-    def set_paths_in_app(self):
+    def __set_endpoints(self):
         for available_endpoint in self.available_endpoints:
             available_endpoint: AvailableEndpoint
-            available_endpoint.set_callback_name()
+            available_endpoint.handler_data_callback()
 
             self.app.add_url_rule(
                 available_endpoint.url, 
-                view_func=available_endpoint.get_json_callback)
+                view_func=available_endpoint.data_callback)
+
+    def __main_view(self):
+        return flask.render_template("index.html", available_endpoints=self.available_endpoints)
+
+    def __set_main_view(self):
+        self.app.add_url_rule("/", view_func=self.__main_view)
